5285ca53bec627579878705a678189cfa89f4be5
Sergio <sergio.fontechaortola@altran.com>
2018-10-28 12:50:00 +0100

    adding folders src & e2e

---
+++ src/app/courses-list/courses-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import gql from 'graphql-tag';
+
+import { Course, Query } from '../types';
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.css']
+})
+export class CoursesListComponent implements OnInit {
+  courses: Observable<Course[]>;
+
+  constructor( private apollo: Apollo ) { }
+
+  ngOnInit() {
+    this.courses = this.apollo.watchQuery<Query>({
+      query: gql `
+        query allCourses {
+          allCourses {
+            id
+            title
+            author
+            description
+            topic
+            url
+          }
+        }
+      `
+    })
+     .valueChanges
+     .pipe(
+       map(result => result.data.allCourses)
+      );
+  }
+}
